9aea792bab8c96361568dd318cba22e4da65e86b
LI Qing <geding.lq@alibaba-inc.com>
2020-01-13 05:54:09 +0000

    Refactor scripts that compiles and installs SGX SDK

--- compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make clean
-make
-make sdk_install_pkg psw_install_pkg
---
+++ compile_and_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+pushd `dirname $0` > /dev/null
+SCRIPT_PATH=`pwd`
+popd > /dev/null
+
+# Uninstall and clean up
+sudo /opt/intel/sgxsdk/uninstall.sh
+sudo /opt/intel/sgxpsw/uninstall.sh
+make clean
+
+# Compile SDK and install
+make sdk
+make sdk_install_pkg
+sudo mkdir -p /opt/intel
+cd /opt/intel
+yes yes | sudo ${SCRIPT_PATH}/linux/installer/bin/sgx_linux_x64_sdk_*.bin
+
+# Compile PSW and install
+# Note that the compilation of PSW requires the installation of SDK.
+cd ${SCRIPT_PATH}
+make psw
+make psw_install_pkg
+cd /opt/intel
+sudo ${SCRIPT_PATH}/linux/installer/bin/sgx_linux_x64_psw_*.bin
