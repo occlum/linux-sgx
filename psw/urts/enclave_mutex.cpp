d81dde073ba51af1333d7c39c162a66da437c875
Tate, Hongliang Tian <tate.thl@antfin.com>
2020-07-03 13:32:36 +0000

    Add optional timeout to the OCall for waiting events

--- psw/urts/enclave_mutex.cpp
+++ psw/urts/enclave_mutex.cpp
@@ -52,7 +52,7 @@ extern "C" int sgx_thread_wait_untrusted_event_ocall(const void *self)
     return SGX_SUCCESS;
 }
 
-extern "C" int sgx_thread_wait_untrusted_event_timeout_ocall(const void *self, int64_t sec, int64_t nsec, int *err)
+extern "C" int sgx_thread_wait_untrusted_event_timeout_ocall(const void *self, const struct timespec *ts, int *err)
 {
     if (self == NULL)
         return SGX_ERROR_INVALID_PARAMETER;
@@ -61,8 +61,7 @@ extern "C" int sgx_thread_wait_untrusted_event_timeout_ocall(const void *self, i
     if (hevent == NULL)
         return SE_ERROR_MUTEX_GET_EVENT;
 
-    struct timespec ts = {.tv_sec = sec, .tv_nsec = nsec};
-    if (SE_MUTEX_SUCCESS != se_event_timeout_wait(hevent, &ts, err))
+    if (SE_MUTEX_SUCCESS != se_event_timeout_wait(hevent, ts, err))
         return SE_ERROR_MUTEX_WAIT_EVENT;
 
     return SGX_SUCCESS;
